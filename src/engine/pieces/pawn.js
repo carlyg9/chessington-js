d80e56230e83026894b0fa38e60a5b8899a2ed8f
root <root@intcargil.zoo.lan>
2019-07-08 14:03:51 +0100

    Made pawns move

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+import Player from '../player.js';
 
 export default class Pawn extends Piece {
     constructor(player) {
@@ -6,6 +8,18 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = [];
+        let currentLocation = board.findPiece(this);
+        if (this.player === Player.WHITE){
+            availableMoves.push(Square.at((currentLocation.row +1), currentLocation.col));
+            return availableMoves;
+        }
+        else if (this.player === Player.BLACK){
+            availableMoves.push(Square.at((currentLocation.row -1), currentLocation.col));
+            return availableMoves;
+        }
+        else {
+            console.log('Not a valid colour.');
+        }
     }
 }
