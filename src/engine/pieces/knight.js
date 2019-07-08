840747e7f7b94a3f64fda37f048fd3fbbef56334
root <root@intcargil.zoo.lan>
2019-07-08 15:46:43 +0100

    Made knight move

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -6,6 +6,15 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let availableMoves = [];
+        let currentSquare = board.findPiece(this);
+        const arrayPlusMinus = [1, -1];
+        for (let i of arrayPlusMinus){
+            for (let j of arrayPlusMinus){
+                availableMoves = this.addMove(availableMoves, currentSquare, i*1, j*2);
+                availableMoves = this.addMove(availableMoves, currentSquare, i*2, j*1);
+            }
+        }
+        return availableMoves;
     }
 }
