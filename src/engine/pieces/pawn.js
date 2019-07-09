16ed9a9a4d9ad60f328fd91ed33b0038d835ab80
root <root@intcargil.zoo.lan>
2019-07-09 13:54:30 +0100

    pawn can take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,55 +1,80 @@
 import Piece from './piece';
 import Square from '../square';
 import Player from '../player.js';
+import King from './king';
 
 export default class Pawn extends Piece {
     constructor(player) {
         super(player);
     }
-    whitePawnMovement(availableMoves, currentSquare){
-        if (this.player === Player.WHITE){
-            if (currentSquare.row === 1){
-                availableMoves = this.addMove(availableMoves, currentSquare, 1, 0);
-                availableMoves = this.addMove(availableMoves, currentSquare, 2, 0);
-            }
-            else if (currentSquare.row === 7){
-                return availableMoves;
-            }
-            else {
-                availableMoves = this.addMove(availableMoves, currentSquare, 1, 0);
-            }
+    whitePawnMovement(board, availableMoves, currentSquare){
+        if (currentSquare.row === 1){
+            availableMoves = this.addMove(availableMoves, currentSquare, 1, 0);
+            availableMoves = this.addMove(availableMoves, currentSquare, 2, 0);
+        }
+        else if (currentSquare.row === 7){
+            return availableMoves;
+        }
+        else {
+            availableMoves = this.addMove(availableMoves, currentSquare, 1, 0);
+        }
 
+        if (availableMoves.length > 0 && board.getPiece(availableMoves[0])){
+            availableMoves = [];
+        }
+        else if (availableMoves.length === 2 && board.getPiece(availableMoves[1])){
+            availableMoves = [availableMoves[0]];
+        }
+        let NorthEast = Square.at(currentSquare.row +1, currentSquare.col +1);
+        if (!!board.getPiece(NorthEast) && !(board.getPiece(NorthEast).player === this.player) && ! (board.getPiece(NorthEast) instanceof King)) {
+            availableMoves.push(NorthEast);
+        }
+        let NorthWest = Square.at(currentSquare.row +1, currentSquare.col -1);
+        if (!!board.getPiece(NorthWest) && !(board.getPiece(NorthWest).player === this.player) && ! (board.getPiece(NorthWest) instanceof King)) {
+            availableMoves.push(NorthWest);
         }
         return availableMoves
     }
 
-    blackPawnMovement(availableMoves, currentSquare){
-        if (this.player === Player.BLACK){
-            if (currentSquare.row === 6){
-                availableMoves = this.addMove(availableMoves, currentSquare, -1, 0);
-                availableMoves = this.addMove(availableMoves, currentSquare, -2, 0);
-            }
-            else if (currentSquare.row === 0){
-                return availableMoves;
-            }
-            else {
-                availableMoves = this.addMove(availableMoves, currentSquare, -1, 0);
-            }
-
+    blackPawnMovement(board, availableMoves, currentSquare){
+        if (currentSquare.row === 6){
+            availableMoves = this.addMove(availableMoves, currentSquare, -1, 0);
+            availableMoves = this.addMove(availableMoves, currentSquare, -2, 0);
         }
+        else if (currentSquare.row === 0){
+            return availableMoves;
+        }
+        else {
+            availableMoves = this.addMove(availableMoves, currentSquare, -1, 0);
+        }
+
+        if (availableMoves.length > 0 && board.getPiece(availableMoves[0])){
+            availableMoves = [];
+        }
+        else if (availableMoves.length === 2 && board.getPiece(availableMoves[1])){
+            availableMoves = [availableMoves[0]];
+        }
+
+        let SouthEast = Square.at(currentSquare.row -1, currentSquare.col +1);
+        if (!!board.getPiece(SouthEast) && !(board.getPiece(SouthEast).player === this.player) && ! (board.getPiece(SouthEast) instanceof King)) {
+            availableMoves.push(SouthEast);
+        }
+        let SouthWest = Square.at(currentSquare.row -1, currentSquare.col -1);
+        if (!!board.getPiece(SouthWest) && !(board.getPiece(SouthWest).player === this.player) && ! (board.getPiece(SouthWest) instanceof King)) {
+            availableMoves.push(SouthWest);
+        }
+
         return availableMoves
     }
 
     getAvailableMoves(board) {
         let availableMoves = [];
         let currentSquare = board.findPiece(this);
-        availableMoves = this.whitePawnMovement(availableMoves, currentSquare);
-        availableMoves = this.blackPawnMovement(availableMoves, currentSquare);
-        if (availableMoves.length > 0 && board.getPiece(availableMoves[0])){
-            availableMoves = [];
+        if (this.player === Player.WHITE){
+            availableMoves = this.whitePawnMovement(board, availableMoves, currentSquare);
         }
-        else if (availableMoves.length === 2 && board.getPiece(availableMoves[1])){
-            availableMoves = [availableMoves[0]];
+        else if (this.player === Player.BLACK){
+            availableMoves = this.blackPawnMovement(board, availableMoves, currentSquare);
         }
         return availableMoves;
     }
